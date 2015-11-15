11857e239c7e9ffd283b80f081f26cb75f770e10
kimamula <kenji.imamula@gmail.com>
2015-11-15 15:31:17 +0900

    Publish subject as observable

--- ts/actions/TodoActions.ts
+++ ts/actions/TodoActions.ts
@@ -3,35 +3,36 @@ import {Todo, TodoState} from '../stores/TodoStore';
 import * as Rx from 'rx';
 
 export default class TodoActions {
-    observable = new Rx.Subject<(state: TodoState) => TodoState>();
+    private subject = new Rx.Subject<(state: TodoState) => TodoState>();
+    observable = this.subject.asObservable();
     constructor(private todoReducers: TodoReducers) {
     }
     create(text: string): void {
-        this.observable.onNext(this.todoReducers.for('create').applyPayload({text}));
+        this.subject.onNext(this.todoReducers.for('create').applyPayload({text}));
     }
 
     updateText(index: number, text: string): void {
-        this.observable.onNext(this.todoReducers.for('updateText').applyPayload({index, text}));
+        this.subject.onNext(this.todoReducers.for('updateText').applyPayload({index, text}));
     }
 
     toggleComplete(index: number, todo: Todo): void {
         if (todo.isCompleted()) {
-            this.observable.onNext(this.todoReducers.for('undoComplete').applyPayload({index}));
+            this.subject.onNext(this.todoReducers.for('undoComplete').applyPayload({index}));
         } else {
-            this.observable.onNext(this.todoReducers.for('complete').applyPayload({index}));
+            this.subject.onNext(this.todoReducers.for('complete').applyPayload({index}));
         }
     }
 
     toggleCompleteAll(): void {
-        this.observable.onNext(this.todoReducers.for('toggleCompleteAll').applyPayload({}));
+        this.subject.onNext(this.todoReducers.for('toggleCompleteAll').applyPayload({}));
     }
 
     destroy(index: number): void {
-        this.observable.onNext(this.todoReducers.for('destroy').applyPayload({index}));
+        this.subject.onNext(this.todoReducers.for('destroy').applyPayload({index}));
     }
 
     destroyCompleted(): void {
-        this.observable.onNext(this.todoReducers.for('destroyCompleted').applyPayload({}));
+        this.subject.onNext(this.todoReducers.for('destroyCompleted').applyPayload({}));
     }
 
 }
